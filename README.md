909161cbac46f5910ec19317376d5fe8b71a6a3f
orfvl <oscarvonloehneysen@gmail.com>
2022-12-20 09:53:58 +0100

    good commit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Front-insertion
 Benchmarking insertion into the front of containers in C++ using google benchmark
+
+To run benchmark create build directory, in build directory run `cmake ..` `cmake --build .` and then run `./main --benchmark_out="test_data.json" --benchmark_format=json`
+
+To plot results run `python plotdata.py`
--- main.cpp
+++ main.cpp
@@ -11,13 +11,12 @@ static void bm_vec(benchmark::State &state) {
         // rng.seed(std::random_device()());
         // std::uniform_int_distribution<int> dist(0,255);
         std::vector<int> v ;
-        //v.reserve(1);
-        //v.reserve(state.range(0));
         benchmark::DoNotOptimize(v.data());
+        v.reserve(1);
+        //v.reserve(state.range(0));
         state.ResumeTiming();
         for (int i = 0; i < state.range(0); i++)
         {
-           
             v.insert(v.begin(), 1);
         }
         benchmark::ClobberMemory();
@@ -26,7 +25,7 @@ static void bm_vec(benchmark::State &state) {
 
 BENCHMARK(bm_vec)
 //->RangeMultiplier(2)->Range(8, 8<<10)->DenseRange(1000,100000,1000);
-->DenseRange(10,2000,10);
+->DenseRange(20,2000,20);
 // static void bm_vec_rev(benchmark::State &state) {
 //     for (auto _ : state)
 //     {
@@ -63,7 +62,7 @@ static void bm_list(benchmark::State &state) {
     }
 }
 
-BENCHMARK(bm_list)->DenseRange(10,2000,10);
+BENCHMARK(bm_list)->DenseRange(20,2000,20);
 //->RangeMultiplier(2)->Range(8, 8 << 10) ->DenseRange(1000,100000,1000);
 
 
